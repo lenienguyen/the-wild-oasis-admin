bc72e82aa7ddcefe7599c1de14e4d3d3a178df3b
lenienguyen <lenie.nguy10@gmail.com>
2025-07-30 21:59:21 +0200

    Add more reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Row from "./ui/Row";
 
 const StyledApp = styled.div`
   padding: 20px;
-  background-color: orangered;
 `;
 
 const App = () => {
@@ -15,21 +14,25 @@ const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-          <div>
-            <Heading as="h2">Check in out</Heading>
-            <Button>Check in</Button>
-            <Button>Check out</Button>
-          </div>
-        </Row>
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form action="">
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button variation="secondary" size="small">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
