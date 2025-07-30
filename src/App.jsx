fc122fa336dd0eb332ffca1a94b5c25cde1fb2a5
lenienguyen <lenie.nguy10@gmail.com>
2025-07-30 21:50:52 +0200

    Testing a few styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  padding: 20px;
+  background-color: orangered;
+`;
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+
+          <div>
+            <Heading as="h2">Check in out</Heading>
+            <Button>Check in</Button>
+            <Button>Check out</Button>
+          </div>
+        </Row>
+        <Row type="vertical">
+          <Heading as="h3">Form</Heading>
+          <form action="">
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </StyledApp>
+    </>
+  );
 };
 
 export default App;
