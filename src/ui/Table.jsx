a4ce8be51cbdd888b23efff16cf73569c50d77f2
lenienguyen <lenie.nguy10@gmail.com>
2025-08-02 23:00:16 +0200

    Finish table with render

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -90,7 +90,11 @@ const Row = ({ children }) => {
   );
 };
 
-const Body = ({ children }) => {};
+const Body = ({ data, render }) => {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+
+  return <StyledBody>{data.map(render)}</StyledBody>;
+};
 
 Table.Header = Header;
 Table.Row = Row;
