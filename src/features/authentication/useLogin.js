a065b143d505c602a548fd631df7d34aedd91a30
lenienguyen <lenie.nguy10@gmail.com>
2025-08-03 19:16:26 +0200

    feat(auth): user login with db

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export const useLogin = () => {
+  const navigate = useNavigate();
+
+  const { mutate: login, isPending } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      navigate("/");
+    },
+    onError: (err) => {
+      console.log("Error", err);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isPending };
+};
