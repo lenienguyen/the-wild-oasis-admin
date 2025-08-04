3e0c6bc9bed48f4beac00927062e00a4b6b07dac
lenienguyen <lenie.nguy10@gmail.com>
2025-08-04 21:52:37 +0200

    Clearlog

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState("lenie@example.com");
-  const [password, setPassword] = useState("password10");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isPending: isLoading } = useLogin();
 
   const handleSubmit = (e) => {
